f45f04c1bc700d9c3465357d6ad55838d02e7414
Paul Cabezal <paulcabezal@gmail.com>
2022-05-18 12:37:01 -0400

    grid mouseenter function working

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const mainContainer = document.querySelector('.mainContainer');
 
-//yAxis = document.querySelectorAll('.yAxis'); 
 const addyAxis = document.createElement('div');
 addyAxis.classList.add('yAxis');
 
-// const block = document.querySelectorAll('.block'); maybe not needed?
 const addBlock = document.createElement('div');
 addBlock.classList.add('block');
 
@@ -22,7 +20,37 @@ function createGrid() {
 
 createGrid();
 
-/* mainContainer.appendChild(addyAxis);
+const block1 = document.querySelectorAll('.block');
+
+for(let i = 0; i < block1.length; i++) {
+    block1[i].addEventListener("mouseenter", bindClick(i));
+}
+
+function bindClick(i) {
+ return function() {
+ //   console.log("you entered region number " + i);
+    block1[i].style.backgroundColor = 'red';
+ };
+}
+
+
+/* console.log(block)
+console.log(block1)
+console.log(block2);
+
+block.addEventListener("mouseenter", changeColor);
+
+//block.addEventListener("mouseenter", changeColor);
+//console.log(block);
+
+function changeColor() {
+    console.log('sup bro');
+    block.style.backgroundColor = 'red';
+    addBlock.classList.add('dude');
+
+};
+
+ mainContainer.appendChild(addyAxis);
 addyAxis.appendChild(addBlock.cloneNode(true));
 addyAxis.appendChild(addBlock.cloneNode(true));
 addyAxis.appendChild(addBlock.cloneNode(true));
